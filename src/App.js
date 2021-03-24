71c9cf972e9bb0ee859387eaccc6601f9264a67c
Tim Samuel <timmatsam@gmail.com>
2021-03-23 21:05:26 -0400

    changed package.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import Calculator from "./Calculator";
 import About from "./About";
-import { Canvas } from "react-three-fiber";
 
 function App() {
   return (
     <div id="app">
       <Calculator />
       <About />
-      {/* <Canvas camera={{ position: [0, 0, 0], fov: 70 }}></Canvas> */}
     </div>
   );
 }
-
 export default App;
